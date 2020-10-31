292192e02b6e293a4d38f87027d3d09a6996e2bf
nelpaubalino <nelpaubalino@gmail.com>
2020-11-01 01:21:39 +1100

    update

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -31,7 +31,7 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
-
+# HOME
 @app.route("/")
 def home():
     """List all available api routes."""
@@ -40,45 +40,75 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"-------------------------<br/>"
+        f"Get temperature data from the date provided as yyyy-mm-dd <br/>"
+         f"-------------------------<br/>"
+        f"/api/v1.0/yyyy-mm-dd<br/>"
+         f"-------------------------<br/>"
+        f"Get temperature data from the date provided as yyyy-mm-dd to 2nd provided date.<br/>"
+         f"-------------------------<br/>"
+        f"/api/v1.0/yyyy-mm-dd/yyyy-mm-dd"
     )
 
+# Precipitation
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
     latest_date = dt.date(2017, 8, 23)
     last_year = dt.timedelta(days=365)
     latest_year = latest_date - last_year
-    
     measure_data = [Measurement.id, Measurement.date, Measurement.prcp, Measurement.station]
     get_data = session.query(*measure_data).filter(Measurement.date >= latest_year).all()
     session.close()
-    
-    precipitation = list(np.ravel(get_data))
 
     return jsonify(get_data)
 
+# Stations
 @app.route("/api/v1.0/stations")
 def stations():
+    session = Session(engine)
+    station_data = [Station.id, Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation]
+    get_data = session.query(*station_data).all()
+    session.close()
     
-    return()
+    return jsonify(get_data)
 
+# Tobs
 @app.route("/api/v1.0/tobs")
 def tobs():
-    
-    return()
+    session = Session(engine)
+    max_temp = session.query(func.max(Measurement.tobs)).filter(Measurement.station == "USC00519281").all()
+    min_temp = session.query(func.min(Measurement.tobs)).filter(Measurement.station == "USC00519281").all()
+    avg_temp = session.query(func.avg(Measurement.tobs)).filter(Measurement.station == "USC00519281").all()
+    result = f"The Max Temperature {max_temp}, Min Temperature {min_temp}, Avg Temperature {avg_temp} of USC00519281 WAIHEE 837.5, HI US."
+    session.close()
 
+    return(result)
+
+# Start
 @app.route("/api/v1.0/<start>")
-def start():
+def start_date(start):
+    session = Session(engine)
+    measure_data = [Measurement.id, Measurement.date, Measurement.prcp, Measurement.station]
+    tmax = session.query(func.max(Measurement.tobs)).filter(Measurement.date >= start).all()
+    tmin = session.query(func.min(Measurement.tobs)).filter(Measurement.date >= start).all()
+    tavg = session.query(func.avg(Measurement.tobs)).filter(Measurement.date >= start).all()
+    result = f"The Max Temperature {tmax}, Min Temperature {tmin}, Avg Temperature {tavg} from the date {start} to 2017, 8, 23."
+    session.close()
     
-    return()
-
+    return jsonify(result)
+# Start to END
 @app.route("/api/v1.0/<start>/<end>")
-def startend():
+def start_to_end(start,end):
+    session = Session(engine)
+    measure_data = [Measurement.id, Measurement.date, Measurement.prcp, Measurement.station]
+    tmax = session.query(func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    tmin = session.query(func.min(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    tavg = session.query(func.avg(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    result = f"The Max Temperature {tmax}, Min Temperature {tmin}, Avg Temperature {tavg} from the date {start} to {end}."
+    session.close()
     
-    return()
-
+    return jsonify(result)
 
 # Define main behavior
 if __name__ == "__main__":
